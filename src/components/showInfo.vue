d8743f10dce8ebc838d984b20b4b0a93a45af705
thatzita <robinudehn@gmail.com>
2018-04-13 09:52:44 +0200

    js och css

--- src/components/showInfo.vue
+++ src/components/showInfo.vue
@@ -1,39 +1,40 @@
 <template >
-  <div class="focus">
+<div class="focus">
 
-    <img v-bind:src="show.imageUrl" alt="">
-    <h2>{{show.songName}}  {{show.songGenre}}  {{show.songYear}}</h2>
-
+  <img v-bind:src="show.imageUrl" alt="">
+  <div class="info">
+    <h4>{{show.songTitle}}</h4>
+    <h4>{{show.songArtist}}</h4>
+    <h4>{{show.songGenre}}</h4>
   </div>
-
-
+</div>
 </template>
 
 <script>
 export default {
-  props:["moreInfo", "dell"],
-  data: function(){
+  props: ["moreInfo", "dell"],
+  data: function() {
     return {
 
-        show:{
-          songName : "",
-          songGenre: "",
-          songYear: "",
-          imageUrl: "",
-        },
+      show: {
+        songTitle: "",
+        songArtist: "",
+        songGenre: "",
+        imageUrl: "",
+      },
 
     }
   },
-  methods :{
-    showObj:function(del){
+  methods: {
+    showObj: function(del) {
 
 
 
 
-          this.show.songName = this.moreInfo.songName;
-          this.show.songGenre = this.moreInfo.songGenre;
-          this.show.songYear = this.moreInfo.songYear;
-          this.show.imageUrl = this.moreInfo.imageUrl;
+      this.show.songTitle = this.moreInfo.songTitle;
+      this.show.songArtist = this.moreInfo.songArtist;
+      this.show.songGenre = this.moreInfo.songGenre;
+      this.show.imageUrl = this.moreInfo.imageUrl;
 
 
 
@@ -44,18 +45,34 @@ export default {
 </script>
 
 <style scoped>
-
-.focus{
+.focus {
   display: inline-block;
-  position:relative;
-  padding: 10px;
-  top:50px;
-  width: 50%;
-  color:black;
+  position: relative;
+  top: 180px;
+  color: black;
+
 }
 
-.focus img{
-  width: 90%;
-  box-shadow: 0 0 10px rgba(0,0,0,0.6);
+.info {
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.6);
+  color: rgba(255, 255, 255, 0.7);
+  text-align: left;
+  padding: 10px 0;
+}
+
+.info h4 {
+  margin-left: 20px;
+}
+
+.focus img {
+  max-width: 500px;
+  overflow: hidden;
+  height: 350px;
+
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
 }
 </style>
